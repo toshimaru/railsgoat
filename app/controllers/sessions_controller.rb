a39b3964cf96ffe6d4a71c1f36edc497989e01ee
toshimaru <me@toshimaru.net>
2021-04-09 15:14:38 +0900

    Test2: Fix error message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -25,7 +25,7 @@ class SessionsController < ApplicationController
       end
       redirect_to path
     else
-      flash[:error] = e.message
+      flash[:error] = "Incorrect Email or Password"
       render "sessions/new"
     end
   end
